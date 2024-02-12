0ba457aa8e725e28017b6a54e95e90b3bbfdbb17
Halil İbrahim <halil.ali.gs.1430@gmail.com>
2024-02-12 15:09:58 +0300

    Project Euler P3 Ekleme.

---
+++ src/Project_Euler_P3.java
@@ -0,0 +1,27 @@
+public class Project_Euler_P3 {
+    public static void main(String[] args) {
+        // Largest Prime Factor
+
+        long i =2L; //divide number
+        long j =600851475143L;// this is our evil number which we must find its largest prime factor
+        long k =3L; // prime number
+
+        while (j>1){ // we need to loop for checking factors
+            if ( j%i ==0){
+                k=i; // this is prime number assignment
+                j=j/i; // new number assignment for dividing again
+
+            }
+            else // in this part we increase our divide number also prime number value
+                if (j%2==1) { // this part only for 2 to 3 transition because after 3 we just add 2 for each else
+                    i += 1;
+                }
+                else
+                    i+=2; // after 3 we just add 2 for each else
+
+        }
+
+
+        System.out.println("Largest Prime Factor İs : "+ k); // output k is prime number
+    }
+}
