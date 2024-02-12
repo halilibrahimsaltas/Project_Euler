0f85f2c9c94ac2b84142eb43243e67e9ed351000
Halil İbrahim <halil.ali.gs.1430@gmail.com>
2024-02-12 15:12:12 +0300

    Project Euler P3 düzenleme

--- src/Project_Euler_P3.java
+++ src/Project_Euler_P3.java
@@ -2,7 +2,7 @@ public class Project_Euler_P3 {
     public static void main(String[] args) {
         // Largest Prime Factor
 
-        long i =2L; //divide number
+        long i =2L; //divisor number
         long j =600851475143L;// this is our evil number which we must find its largest prime factor
         long k =3L; // prime number
 
@@ -12,7 +12,7 @@ public class Project_Euler_P3 {
                 j=j/i; // new number assignment for dividing again
 
             }
-            else // in this part we increase our divide number also prime number value
+            else // in this part we increase our divisor number also prime number value
                 if (j%2==1) { // this part only for 2 to 3 transition because after 3 we just add 2 for each else
                     i += 1;
                 }
